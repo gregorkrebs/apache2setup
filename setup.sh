2f28f83fbb0273bf962677e8e85bba984f2b8f41
Gregor Krebs <101549118+gregorkrebs@users.noreply.github.com>
2023-02-01 09:19:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt ugrade
 sudo apt install ca-certificates apt-transport-https software-properties-common
 sudo add-apt-repository ppa:ondrej/php
-sudo apt install apache2 curl php php8.3 libapache2-mod-php8.2 php-apcu php-common php-curl php-gd php-gmp php-imagick php-intl php-json php-mbstring php-memcache php-mysql php-zip php8.2-apcu php8.2-bcmath php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-gmp php8.2-imagick php8.2-intl php8.2-mbstring php8.2-memcache php8.2-mysql php8.2-opcache php8.2-phpdbg php8.2-readline php8.2-xml php8.2-zip php8.2 mariadb-server mariadb-client
+sudo apt install apache2 curl php php8.1 libapache2-mod-php8.1 php-apcu php-common php-curl php-gd php-gmp php-imagick php-intl php-json php-mbstring php-memcache php-mysql php-zip php8.1-apcu php8.1-bcmath php8.1-cli php8.1-common php8.1-curl php8.1-gd php8.1-gmp php8.1-imagick php8.1-intl php8.1-mbstring php8.1-memcache php8.1-mysql php8.1-opcache php8.1-phpdbg php8.1-readline php8.1-xml php8.1-zip php8.1 mariadb-server mariadb-client
 echo "SUCCESSINFO: installing requirements completed"
 echo "INFO: enable required and recommended mods"
 a2dissite 000-default
@@ -28,7 +28,7 @@ a2enmod macro
 a2enmod mime
 a2enmod mpm_prefork
 a2enmod negotiation
-a2enmod php8.2
+a2enmod php8.1
 a2enmod proxy
 a2enmod proxy_fdpass
 a2enmod proxy_html
