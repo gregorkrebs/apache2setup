ea211546fc0102128ffc993dbceaedde30bfbdd3
Gregor Krebs <101549118+gregorkrebs@users.noreply.github.com>
2023-02-01 09:57:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ sudo apt update
 sudo apt ugrade
 sudo apt install ca-certificates apt-transport-https software-properties-common
 sudo add-apt-repository ppa:ondrej/php
-sudo apt install apache2 curl php php8.1 php8.1-fpm libapache2-mod-php8.1 php-apcu php-common php-curl php-gd php-gmp php-imagick php-intl php-json php-mbstring php-memcache php-mysql php-zip php8.1-apcu php8.1-bcmath php8.1-cli php8.1-common php8.1-curl php8.1-gd php8.1-gmp php8.1-imagick php8.1-intl php8.1-mbstring php8.1-memcache php8.1-mysql php8.1-opcache php8.1-phpdbg php8.1-readline php8.1-xml php8.1-zip php8.1 mariadb-server mariadb-client
+sudo apt install apache2 curl php php-apcu php-common php-curl php-gd php-gmp php-imagick php-intl php-json php-mbstring php-memcache php-mysql php-zip mariadb-server mariadb-client
 sudo apt install php7.4 php7.4-fpm libapache2-mod-php7.4 php7.4-apcu php7.4-bcmath php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-gmp php7.4-imagick php7.4-intl php7.4-mbstring php7.4-memcache php7.4-mysql php7.4-opcache php7.4-phpdbg php7.4-readline php7.4-xml php7.4-zip php7.4
+sudo apt install php8.1 php8.1-fpm libapache2-mod-php8.1 php8.1-apcu php8.1-bcmath php8.1-cli php8.1-common php8.1-curl php8.1-gd php8.1-gmp php8.1-imagick php8.1-intl php8.1-mbstring php8.1-memcache php8.1-mysql php8.1-opcache php8.1-phpdbg php8.1-readline php8.1-xml php8.1-zip php8.1
 sudp apt install php8.2 php8.2-fpm libapache2-mod-php8.2 php8.2-apcu php8.2-bcmath php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-gmp php8.2-imagick php8.2-intl php8.2-mbstring php8.2-memcache php8.2-mysql php8.2-opcache php8.2-phpdbg php8.2-readline php8.2-xml php8.2-zip php8.2
-echo "SUCCESSINFO: installing requirements completed"
-echo "INFO: enable required and recommended mods"
+echo "[INFO] enable required and recommended mods"
 a2dissite 000-default
 a2dissite default-ssl
 a2enmod access_compat
@@ -48,15 +48,16 @@ a2enmod ssl
 a2enmod status
 a2enmod vhost_alias
 a2enmod xml2enc
-echo "INFO: copy configuration files"
+echo "[INFO] Copy configuration files"
 cp conf/hosts.conf /etc/apache2/sites-available/hosts.conf
 cp conf/VHosts.conf /etc/apache2/conf-availble/VHosts.conf
 cp conf/SSLVHosts.conf /etc/apache2/conf-available/SSLVHosts.conf
 cp conf/Proxys.conf /etc/apache2/conf-available/Proxys.conf
-echo "INFO: enable config and vhosts-file"
-a2ensite hosts
+echo "[INFO] Enable config and vhosts-file"
+a2ensite hosts.conf
 a2enconf VHosts.conf
 a2enconf SSLVHosts.conf
 a2enconf Proxys.conf
+echo "[INFO] Restart apache2"
 systemctl restart apache2
-echo "INFO: Installation successfully completed"
+echo "[INFO] Installation successfully completed"
