3080e537ddc4b2da0c77a28eeb8ce1b61e16fba0
Gregor Krebs <101549118+gregorkrebs@users.noreply.github.com>
2024-01-04 09:48:35 +0100

    Update setup-debian.sh
    add php8.3

--- setup-debian.sh
+++ setup-debian.sh
@@ -8,6 +8,7 @@ sudo apt install php7.4 php7.4-fpm libapache2-mod-php7.4 php7.4-apcu php7.4-bcma
 sudo apt install php8.0 php8.0-fpm libapache2-mod-php8.0 php8.0-apcu php8.0-bcmath php8.0-cli php8.0-common php8.0-curl php8.0-gd php8.0-gmp php8.0-imagick php8.0-intl php8.0-mbstring php8.0-memcache php8.0-mysql php8.0-opcache php8.0-phpdbg php8.0-readline php8.0-xml php8.0-zip -y
 sudo apt install php8.1 php8.1-fpm libapache2-mod-php8.1 php8.1-apcu php8.1-bcmath php8.1-cli php8.1-common php8.1-curl php8.1-gd php8.1-gmp php8.1-imagick php8.1-intl php8.1-mbstring php8.1-memcache php8.1-mysql php8.1-opcache php8.1-phpdbg php8.1-readline php8.1-xml php8.1-zip -y
 sudo apt install php8.2 php8.2-fpm libapache2-mod-php8.2 php8.2-apcu php8.2-bcmath php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-gmp php8.2-imagick php8.2-intl php8.2-mbstring php8.2-memcache php8.2-mysql php8.2-opcache php8.2-phpdbg php8.2-readline php8.2-xml php8.2-zip -y
+sudo apt install php8.3 php8.3-fpm libapache2-mod-php8.3 php8.3-apcu php8.3-bcmath php8.3-cli php8.3-common php8.3-curl php8.3-gd php8.3-gmp php8.3-imagick php8.3-intl php8.3-mbstring php8.3-memcache php8.3-mysql php8.3-opcache php8.3-phpdbg php8.3-readline php8.3-xml php8.3-zip -y
 echo "[INFO] enable required and recommended mods"
 a2dissite 000-default
 a2dissite default-ssl
@@ -49,6 +50,7 @@ a2enconf php7.4-fpm
 a2enconf php8.0-fpm
 a2enconf php8.1-fpm
 a2enconf php8.2-fpm
+a2enconf php8.3-fpm
 echo "[INFO] Copy configuration files"
 cp sites/hosts.conf /etc/apache2/sites-available/hosts.conf
 cp sites/LE-template.conf /etc/apache2/sites-available/LE-template.conf
