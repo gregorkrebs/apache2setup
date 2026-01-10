922a828f94bfd49baf27a888721a324cabef9f3b
Gregor Krebs <101549118+gregorkrebs@users.noreply.github.com>
2026-01-11 00:01:54 +0100

    Improve setup script and apache configs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 echo "Easy and Secure Apache2 Webserver Setup"
 echo "[INFO] installing requirements"
 sudo apt update
@@ -20,11 +23,21 @@ case $OS_ID in
 esac
 sudo apt update
 sudo apt install apache2 curl php php-apcu php-common php-curl php-gd php-gmp php-imagick php-intl php-json php-mbstring php-memcache php-mysql php-zip mariadb-server mariadb-client -y
-sudo apt install php7.4 php7.4-fpm libapache2-mod-php7.4 php7.4-apcu php7.4-bcmath php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-gmp php7.4-imagick php7.4-intl php7.4-mbstring php7.4-memcache php7.4-mysql php7.4-opcache php7.4-phpdbg php7.4-readline php7.4-xml php7.4-zip -y
-sudo apt install php8.0 php8.0-fpm libapache2-mod-php8.0 php8.0-apcu php8.0-bcmath php8.0-cli php8.0-common php8.0-curl php8.0-gd php8.0-gmp php8.0-imagick php8.0-intl php8.0-mbstring php8.0-memcache php8.0-mysql php8.0-opcache php8.0-phpdbg php8.0-readline php8.0-xml php8.0-zip -y
-sudo apt install php8.1 php8.1-fpm libapache2-mod-php8.1 php8.1-apcu php8.1-bcmath php8.1-cli php8.1-common php8.1-curl php8.1-gd php8.1-gmp php8.1-imagick php8.1-intl php8.1-mbstring php8.1-memcache php8.1-mysql php8.1-opcache php8.1-phpdbg php8.1-readline php8.1-xml php8.1-zip -y
-sudo apt install php8.2 php8.2-fpm libapache2-mod-php8.2 php8.2-apcu php8.2-bcmath php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-gmp php8.2-imagick php8.2-intl php8.2-mbstring php8.2-memcache php8.2-mysql php8.2-opcache php8.2-phpdbg php8.2-readline php8.2-xml php8.2-zip -y
-sudo apt install php8.3 php8.3-fpm libapache2-mod-php8.3 php8.3-apcu php8.3-bcmath php8.3-cli php8.3-common php8.3-curl php8.3-gd php8.3-gmp php8.3-imagick php8.3-intl php8.3-mbstring php8.3-memcache php8.3-mysql php8.3-opcache php8.3-phpdbg php8.3-readline php8.3-xml php8.3-zip -y
+
+php_versions=(7.4 8.0 8.1 8.2 8.3)
+for version in "${php_versions[@]}"; do
+    if apt-cache show "php$version" >/dev/null 2>&1; then
+        sudo apt install "php$version" "php$version-fpm" "libapache2-mod-php$version" \
+            "php$version-apcu" "php$version-bcmath" "php$version-cli" "php$version-common" \
+            "php$version-curl" "php$version-gd" "php$version-gmp" "php$version-imagick" \
+            "php$version-intl" "php$version-mbstring" "php$version-memcache" \
+            "php$version-mysql" "php$version-opcache" "php$version-phpdbg" \
+            "php$version-readline" "php$version-xml" "php$version-zip" -y
+    else
+        echo "[WARN] PHP $version is not available in this repository, skipping."
+    fi
+done
+
 echo "[INFO] enable required and recommended mods"
 a2dissite 000-default
 a2dissite default-ssl
@@ -52,7 +65,6 @@ a2enmod proxy_fdpass
 a2enmod proxy_html
 a2enmod proxy_http2
 a2enmod proxy_http
-a2enmod proxy
 a2enmod reqtimeout
 a2enmod rewrite
 a2enmod setenvif
@@ -61,12 +73,12 @@ a2enmod ssl
 a2enmod status
 a2enmod vhost_alias
 a2enmod xml2enc
-a2enmod proxy_fcgi setenvif
-a2enconf php7.4-fpm
-a2enconf php8.0-fpm
-a2enconf php8.1-fpm
-a2enconf php8.2-fpm
-a2enconf php8.3-fpm
+a2enmod proxy_fcgi proxy_wstunnel setenvif
+for version in "${php_versions[@]}"; do
+    if [ -e "/etc/apache2/conf-available/php$version-fpm.conf" ]; then
+        a2enconf "php$version-fpm"
+    fi
+done
 echo "[INFO] Copy configuration files"
 cp sites/hosts.conf /etc/apache2/sites-available/hosts.conf
 cp sites/LE-template.conf /etc/apache2/sites-available/LE-template.conf
